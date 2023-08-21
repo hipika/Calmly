24fbed05ba4a382343cbeb5de1a4f5701a3b9cd4
Arnav Choudhary <arnavchoudhary53@gmail.com>
2023-08-20 20:40:20 -0400

    main screen compoenent

---
+++ app/components/GoesToMain.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { ScrollView, StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import SignUpScreen from '../screens/SignUpScreen';
+
+import { NavigationContainer } from '@react-navigation/native';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import MainScreen from '../screens/MainScreen';
+
+const Tabs = createMaterialBottomTabNavigator();
+
+function GoesToMain(navigation) {
+
+    return (
+        
+            // <SafeAreaView style={styles.container}>
+                // <Text>This is the main screen</Text>
+            <NavigationContainer independent={true}>
+                <Tabs.Navigator>
+                    <Tabs.Screen name="Main" component={MainScreen}
+                    options={{
+                        tabBarLabel:"Stuff",
+                        tabBarColor:"red"
+                    }}/>
+
+                </Tabs.Navigator>
+
+
+
+            </NavigationContainer>
+            
+            // </SafeAreaView>
+
+            
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff"
+    },
+
+    quoteText: {
+        color: "black",
+        fontWeight: "bold",
+        fontSize: "15",
+        textAlignVertical: "center",
+        textAlign: "center",
+        justifyContent: "center",
+        alignItems: "center"
+    }
+    
+    
+
+})
+
+export default GoesToMain;
