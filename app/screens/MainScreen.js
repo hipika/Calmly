de84c315c3af4b5a5ef7153dc4d2cdf80879b5e9
Arnav Choudhary <arnavchoudhary53@gmail.com>
2023-08-22 00:08:50 -0400

    testing more navigation

--- app/screens/MainScreen.js
+++ app/screens/MainScreen.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Text, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Image } from "react-native";
+import { Text, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Image, Button } from "react-native";
 
 
+function MainScreen({navigation}) {
 
-
-function MainScreen() {
+    const goToWelcome = () => {
+        navigation.push("Welcome")
+    }
+    
     return (
         <SafeAreaView>
             <Text>
                 Main Screen
+                <Button title="Back to Welcome" onPress={goToWelcome}/>
             </Text>
         </SafeAreaView>
 
