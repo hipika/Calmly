1af1d9fb1d3f8edf3efd172492e716673c771c74
Arnav Choudhary <arnavchoudhary53@gmail.com>
2023-08-22 21:20:24 -0400

    implemented base level email validation

--- app/screens/SignUpScreen.js
+++ app/screens/SignUpScreen.js
@@ -1,21 +1,52 @@
-import React from 'react';
-import { Text, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Image } from "react-native";
+import {React, useState} from 'react';
+import { Text, SafeAreaView, StyleSheet, TouchableWithoutFeedback, Image, View, ScrollView } from "react-native";
 import Input from '../components/Input';
 import SignUp from '../components/SignUp';
+import Login from '../components/Login';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 
 
+
+
 function SignUpScreen(props, navigation) {
+
+        
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [checkValidEmail, setCheckValidEmail] = useState(false);
+
+    const handleCheckEmail = (text) => {
+        let re = /\S+@\S+\.\S+/;
+        let regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4, 6}$/im;
+
+        setEmail(text);
+
+        if (re.test(text) || regex.test(text)) {
+            setCheckValidEmail(false);
+        }
+        else {
+            setCheckValidEmail(true);
+        }
+
+
+    }
+    
+
     return (
-        <SafeAreaView style={styles.container}>
+        <ScrollView contentContainerStyle={styles.container}>
             <Image source={require("../assets/logo.png")} style={styles.image}/>
             <Text style={styles.signUpText}>Sign Up</Text>
             <Input placeholder={"Enter username"} icon={require("../assets/user.png")} iconColor={"#482F2F"}/>
-            <Input placeholder={"Enter email address"} icon={require("../assets/email.png")} iconColor={"#836A6A"}/>
-            <Input placeholder={"Enter password"} icon={require("../assets/lock.png")} iconColor={"#BAB2B2"} security={true}/>
+            <Input placeholder={"Enter email address"} icon={require("../assets/email.png")} iconColor={"#836A6A"} value={email} changeText={(text) => handleCheckEmail(text)}/>
+            <View style={styles.wrongEmailView}>
+                {checkValidEmail ? <Text style={styles.wrongEmailFormat}>Please enter a valid email</Text> : <Text style={styles.wrongEmailFormat}></Text>}
+            </View>
+            <Input placeholder={"Enter password"} icon={require("../assets/lock.png")} iconColor={"#BAB2B2"} security={true} value={pwd}/>
             <Input placeholder={"Confirm password"} icon={require("../assets/lock.png")} iconColor={"#BAB2B2"} security={true}/>
             <SignUp title={"Sign Up"}/>
-        </SafeAreaView>
+
+        </ScrollView>
 
     );
 }
@@ -40,6 +71,16 @@ const styles = StyleSheet.create({
         width: 300,
         height: 300,
         resizeMode: "contain"
+    },
+
+    wrongEmailFormat: {
+        color: "red",
+        fontSize: "8",
+        textAlign: "center"
+    },
+
+    wrongEmailView: {
+        justifyContent: "flex-end"
     }
 
 })
